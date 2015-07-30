ba6ba6a349a0244ea6105dd0a1ac9c697dbbc63d
南漂一卒 <shiy007@qq.com>
2015-07-30 21:16:00 +0800

    【新增】HTML 5 表单元素 输入验证实例方法 Shim（防御 iOS 等系统的奇葩 Bug）

--- iQuery.js
+++ iQuery.js
@@ -2101,10 +2101,13 @@
             iEvent.stopPropagation();
             $_Button.attr('disabled', true);
 
-            $.post(this.action,  this,  function () {
+            if ( this.checkValidity() )
+                $.post(this.action,  this,  function () {
+                    $_Button.prop('disabled', false);
+                    iCallback.call($_Form[0], arguments[0]);
+                });
+            else
                 $_Button.prop('disabled', false);
-                iCallback.call($_Form[0], arguments[0]);
-            });
         });
         $_Button.prop('disabled', false);
 
@@ -2400,6 +2403,52 @@
     );
 
 })(self, self.document, self.iQuery);
+
+
+
+/* ---------- HTML 5 Form Shim ---------- */
+(function ($) {
+
+    function Value_Check() {
+        var $_This = $(this);
+
+        if ($.isData( $_This.attr('required') )  &&  (! this.value))
+            return false;
+
+        var iRegEx = $_This.attr('pattern');
+        if ( $.isData(iRegEx) )  try {
+            return  RegExp(iRegEx).test(this.value);
+        } catch (iError) { }
+
+        if ((this.tagName.toLowerCase() == 'input')  &&  (this.type == 'number')) {
+            var iNumber = Number(this.value),
+                iMin = Number( $_This.attr('min') );
+            if (
+                isNaN(iNumber)  ||
+                (iNumber < iMin)  ||
+                (iNumber > Number( $_This.attr('max') ))  ||
+                ((iNumber - iMin)  %  Number( $_This.attr('step') ))
+            )
+                return false;
+        }
+
+        return true;
+    }
+
+    HTMLInputElement.prototype.checkValidity = Value_Check;
+    HTMLSelectElement.prototype.checkValidity = Value_Check;
+    HTMLTextAreaElement.prototype.checkValidity = Value_Check;
+
+    HTMLFormElement.prototype.checkValidity = function () {
+        var $_Input = $('*[name]:input', this);
+
+        for (var i = 0;  i < $_Input.length;  i++)
+            if (! $_Input[i].checkValidity())
+                return false;
+        return true;
+    };
+
+})(self.iQuery);
 
 
 
